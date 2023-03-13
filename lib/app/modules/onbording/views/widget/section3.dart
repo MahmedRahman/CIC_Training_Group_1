bc4462b1164b44f639852872bad41aceb6d269fc
MohamedRahman <atpfreelancer@gmail.com>
2023-03-13 11:43:32 +0200

    onBording Screen

--- lib/app/modules/onbording/views/widget/section3.dart
+++ lib/app/modules/onbording/views/widget/section3.dart
@@ -1,96 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
-
-  import 'package:flutter/material.dart';
-
-Column section3() {
-    return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(
-                    Icons.arrow_forward,
-                    color: Colors.white,
-                    size: 15,
-                  ),
-                ),
+Widget section3() {
+  return Stack(
+    clipBehavior: Clip.none,
+    children: [
+      Container(
+        height: 120,
+        decoration: BoxDecoration(
+          color: Colors.red,
+        ),
+      ),
+      Positioned(
+        right: 0,
+        left: 0,
+        top: -20,
+        child: Center(
+          child: Positioned(
+            left: 0,
+            child: Center(
+              child: Container(
+                //width: 50,
                 decoration: BoxDecoration(
-                  color: Color(0xff008066),
+                  color: Colors.white,
                   borderRadius: BorderRadius.circular(40),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 20,
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: Color(0xffE79E39).withOpacity(.5),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Opacity(
-                    opacity: .5,
-                    child: Container(
-                      width: 20,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        color: Color(0xffE79E39),
-                        borderRadius: BorderRadius.circular(50),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                        size: 15,
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    width: 20,
-                    height: 20,
                     decoration: BoxDecoration(
-                      color: Color(0xffE79E39),
-                      borderRadius: BorderRadius.circular(50),
+                      color: Color(0xff008066),
+                      borderRadius: BorderRadius.circular(40),
                     ),
                   ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    width: 20,
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: Color(0xffE79E39).withOpacity(.5),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Opacity(
-                    opacity: .5,
-                    child: Container(
-                      width: 20,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        color: Color(0xffE79E39),
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                  )
-                ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: Color(0xffE79E39).withOpacity(.5),
+                  borderRadius: BorderRadius.circular(50),
+                ),
               ),
               SizedBox(
-                height: 24,
+                width: 10,
               ),
-              Text("Skip"),
+              Opacity(
+                opacity: .5,
+                child: Container(
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: Color(0xffE79E39),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: Color(0xffE79E39),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: Color(0xffE79E39).withOpacity(.5),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Opacity(
+                opacity: .5,
+                child: Container(
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: Color(0xffE79E39),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              )
             ],
-          );
-  }
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Text("Skip"),
+        ],
+      ),
+    ],
+  );
+}
