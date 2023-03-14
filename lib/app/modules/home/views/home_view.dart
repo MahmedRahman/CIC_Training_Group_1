aa7b02bae367d6d0d3e2727cc1a9adb5496bf4a8
MohamedRahman <atpfreelancer@gmail.com>
2023-03-14 11:55:37 +0200

    moduling update

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -4,8 +4,15 @@ import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
 
+Product product1 = Product(
+  name: "product name",
+  description: "description",
+  title: "title",
+  image: "asset/image/map.png",
+  price: 200,
+);
+
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -35,9 +42,12 @@ class HomeView extends GetView<HomeController> {
                   height: 250,
                   color: Colors.blue,
                 ),
-                Container(
-                  height: 250,
-                  color: Colors.yellow,
+                productCard(
+                  image: product1.image,
+                  title: product1.title,
+                  Price: product1.price,
+                  decription: product1.description,
+                  name: product1.name,
                 ),
                 section7(
                   text: "Who we Are",
@@ -58,28 +68,147 @@ class HomeView extends GetView<HomeController> {
     );
   }
 
-  Container section3() {
-    return Container(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          "asset/image/slider001.png",
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          "asset/image/slider001.png",
-                        ),
-                      )
-                    ],
+  Widget productCard({
+    required String image,
+    required String name,
+    required String title,
+    required String decription,
+    required int Price,
+  }) {
+    return Card(
+      elevation: 10,
+      child: Container(
+        //height: 250,
+        width: 255,
+        child: Column(children: [
+          Container(
+            width: Get.width,
+            decoration: BoxDecoration(
+              color: Color(0xffE9E9EC),
+            ),
+            child: Image.asset(image),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+              right: 10,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
-              );
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  decription,
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.label,
+                  color: Color(0xffE79E39),
+                ),
+                Text(Price.toString()),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(Icons.shop),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: MaterialButton(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    onPressed: () {},
+                    color: Color(0xff008066),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset("asset/image/Path 59551.png"),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "Donate",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // child: ElevatedButton(
+
+                  //   onPressed: () {},
+                  //   child: Row(
+                  //     mainAxisAlignment: MainAxisAlignment.center,
+                  //     crossAxisAlignment: CrossAxisAlignment.center,
+                  //     children: [
+                  //       Image.asset("asset/image/Path 59551.png"),
+                  //       SizedBox(
+                  //         width: 5,
+                  //       ),
+                  //       Text("Donate"),
+                  //     ],
+                  //   ),
+                  // ),
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+
+  Container section3() {
+    return Container(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                "asset/image/slider001.png",
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                "asset/image/slider001.png",
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   Container section2() {
@@ -198,4 +327,20 @@ class HomeView extends GetView<HomeController> {
       ),
     );
   }
+}
+
+class Product {
+  String name;
+  String description;
+  String title;
+  int price;
+  String image;
+
+  Product({
+    required this.name,
+    required this.description,
+    required this.title,
+    required this.image,
+    required this.price,
+  });
 }
