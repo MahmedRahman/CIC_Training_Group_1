b0c9aa5c2066e5504f4ae0c3094f95ea4f1af93c
MohamedRahman <atpfreelancer@gmail.com>
2023-03-15 08:40:56 +0200

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Misr",
+      title: "appname",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
